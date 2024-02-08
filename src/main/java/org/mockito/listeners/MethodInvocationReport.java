1261de1334215d23d2ad0ee3e33bb3950aaa8244
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-02-08 12:37:04 +0530

    Update MethodInvocationReport.java

--- src/main/java/org/mockito/listeners/MethodInvocationReport.java
+++ src/main/java/org/mockito/listeners/MethodInvocationReport.java
@@ -16,6 +16,7 @@ import org.mockito.invocation.DescribedInvocation;
  * </p>
  */
 public interface MethodInvocationReport {
+    // qaz
     /**
      * @return Information on the method call, never {@code null}
      */
