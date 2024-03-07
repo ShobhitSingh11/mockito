adba6d50ebc6ad62ad6d96c174841f2e6c6363a0
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-03-07 14:55:13 +0530

    Update Answer2.java

--- src/main/java/org/mockito/stubbing/Answer2.java
+++ src/main/java/org/mockito/stubbing/Answer2.java
@@ -31,6 +31,7 @@ package org.mockito.stubbing;
  * @see Answer
  */
 public interface Answer2<T, A0, A1> {
+    // rfrefrr43e4fe3fr3ef
     /**
      * @param argument0 the first argument.
      * @param argument1 the second argument.
